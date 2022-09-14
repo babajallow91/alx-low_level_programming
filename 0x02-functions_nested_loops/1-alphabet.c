53fb40765eae1db48b5e74b1e60bb79e5d314f98
babajallow91 <momodousalieu.jallow@gmail.com>
2022-09-14 00:31:14 +0000

    Nice to Code

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,20 @@
 #include "main.h"
+
 /**
- * print_alphabet - function to print abc
- *
- * Return: 0
+ * print_alphabet - Entry point
+ * Return:0
  */
+
 void print_alphabet(void)
 {
-	char c;
+	char c = 'a';
 
-	for (c = 'a'; c <= 'z'; c++)
+	while (c <= 'z')
 	{
 		_putchar(c);
+		c++;
+
 	}
-_putchar('\n');
+	_putchar('\n');
+
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-Hello_Functions
+PROJECT 0x02. C - Functions, nested loops
