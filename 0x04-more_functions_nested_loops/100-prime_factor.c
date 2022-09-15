b59b4117cd6ede6279a90646dcb19d3fabddffad
babajallow91 <momodousalieu.jallow@gmail.com>
2022-09-15 21:47:03 +0000

    11 and 12 function nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,36 +1,39 @@
-#include "main.h"
 #include <stdio.h>
-/**
- * print_to_98 - Print numbers from n to 98,
- * @n:first integer value
- *
- * Return: the summation.
- */
-void print_to_98(int n)
-{
+#include <math.h>
 
-	if (n < 98)
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int n;
+	long int max;
+	long int i;
+
+	n = 612852475143;
+	max = -1;
+
+	while (n % 2 == 0)
 	{
-	while (n <= 98)
-		{
-	printf("%d", n);
-	if (n != 98)
-	printf(", ");
-		}
-		n++;
+		max = 2;
+		n /= 2;
 	}
-	else if (n >= 98)
+
+	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
-		while (n >= 98)
+		while (n % i == 0)
 		{
-		if (n != 98)
-		printf("%d", n);
-		if(n != 98)
-		printf(", ");
+			max = i;
+			n = n / i;
 		}
-		n--;
 	}
-	else
-	printf("98");
-	printf("\n");
+
+	if (n > 2)
+		max = n;
+
+	printf("%ld\n", max);
+
+	return (0);
 }
