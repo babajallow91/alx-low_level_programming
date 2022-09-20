464a04d4bd8898858d87bbd3d91c686282798443
babajallow91 <momodousalieu.jallow@gmail.com>
2022-09-19 20:21:26 -0700

    forkit

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,19 +3,15 @@
 
 
 
-/*
- *
- * puts_half - prints a string
- *
- * @str: string to print
- *
- * Return: nothing
- *
- */
-
-
-
-void puts_half(char *str)
+/**
+*lf - prints a string
+*
+* @str: puts_half string to print
+*
+* Return: nothing
+*
+*/
+	void puts_half(char *str)
 
 {
 
@@ -24,7 +20,7 @@ void puts_half(char *str)
 		for (i = ((len - 1) / 2) + 1; i < len; i++)
 
 		putchar(*(str + i));
-		putchar(10);
+			putchar(10);
 }
 
 
@@ -35,14 +31,12 @@ void puts_half(char *str)
  *
  * @s: string
  *
- *
  * Return: the length of the given string
  *
  */
 
 
-
-int _strlen(char *s)
+	int _strlen(char *s)
 
 {
 	int len = 0;
