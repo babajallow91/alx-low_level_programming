db4ba810526e9f79858de6cba2b173f9fd2b23df
babajallow91 <momodousalieu.jallow@gmail.com>
2022-09-22 17:12:22 -0700

    fot

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -5,10 +5,11 @@ int main(void)
 	int n;
 	int a[5];
 	int *p;
-		a[2] = 1024;
-		p = &n;
+	
+	a[2] = 1024;
+	p = &n;
   /*
-   * write your line of code here...
+   * write your line of code here...main
    * Remember:
    * - you are not allowed to use a
    * - you are not allowed to modify p
