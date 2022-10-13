ce0d1c96fe40d4940ef56b817171c6f64fef4813
babajallow91 <momodousalieu.jallow@gmail.com>
2022-10-13 04:14:00 -0700

    print name flie update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,16 @@
-/* A function that prints a name */
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: the array that stores the name
- * @f: a function pointer
- * Return: Nothing
- */
+* print_name - prints a name
+* @name: name to print
+* @f: pointer to a function
+*
+* Description: prints a name using function pointers
+* Return: void
+*/
+
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f)
+	if (name != NULL && f != NULL)
 		f(name);
 }
