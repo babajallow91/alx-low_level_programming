f8a5e5023a8cf76016c38639996f127325fe6ab3
babajallow91 <momodousalieu.jallow@gmail.com>
2022-10-12 18:39:19 -0700

    my goal always is good

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,13 +3,14 @@
 /**
 * print_name - prints a name
 * @name: name to print
-* @f: function to print with
+* @f: pointer to a function
+*
 * Description: prints a name using function pointers
 * Return: void
 */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if(name != NULL && f != NULL)
+	if (name != NULL && f != NULL)
 		f(name);
 }
