ad2db84f3f6c05b4fbb63a7e9b9f69fcd0686278
babajallow91 <momodousalieu.jallow@gmail.com>
2022-10-12 18:22:57 -0700

    my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+* print_name - prints a name
+* @name: name to print
+* @f: function to print with
+* Description: prints a name using function pointers
+* Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if(name != NULL && f != NULL)
+		f(name);
+}
